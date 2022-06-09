f3e5ebc9b6bfed8180227f21dfcfc605466413a9
Tim <tim.fink519@gmail.com>
2022-06-09 18:06:45 -0400

    commit tree dab03cb477f17aa3ada8c16acecee053469b037b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove(main);
+const newHeader = document.createElement("h1");
+
+    newHeader.setAttribute("id", "victory");
+    newHeader.innerHTML = 'YOUR-NAME is the champion';
+    newHeader.appendChild(newHeader);
